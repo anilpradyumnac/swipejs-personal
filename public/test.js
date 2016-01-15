10c1d61b5d722ae14079e0adc7d0941ba30a5c0a
anilpradyumnac <anilpradyumnac@gmail.com>
2016-01-15 15:19:23 +0530

    recent additions

--- public/test.js
+++ public/test.js
@@ -1,7 +1,8 @@
-var initial = 0
-var start = {}
+var initial = {};
+var start = {};
 var s;
-start.StartX
+start.StartX;
+var touchobj = {};
 
 //Stepper function added from FRPJS
 function stepper(eventStream, initial) {
@@ -13,12 +14,19 @@ function stepper(eventStream, initial) {
         return valueAtLastStep
     })
 }
-
+function foldp = function(eventStream, step, initial) {
+    return (function(next) {
+        var accumulated = initial
+        eventStream(function (value) {
+            next(accumulated = step(accumulated, value))
+        })
+    })
+}
 //map function from FRPJS
 function map(eventStream, valueTransform) {
     return function(next) {
         eventStream(function(value) {
-            next(valueTransform(value))
+            next(valueTransform(value));
         })
     }
 }
@@ -26,19 +34,20 @@ function map(eventStream, valueTransform) {
 //on function from FRPJS
 function on(element, name, useCapture) {
     return function(next) {
-        element.addEventListener(name, next, !!useCapture)
+        element.addEventListener(name, next, !!useCapture);
     }
 }
 
 function touchMove(element) {
     var ele = document.getElementById(element);
-    var eventStream = on(ele, 'touchmove', false);
-    eventStream(ontouchMove);
-    s  = stepper(eventStream,initial);
+    var event = on(ele, 'touchmove', false);
+    event(ontouchMove);
+    s  = foldp(event, step, initial);
+    
 }
+
 function ontouchMove(e) {
-    start.StartX = e.targetTouches[0].pageX;
-    var touchobj = {};
-    touchobj = s();
-    console.log(touchobj.changedTouches[0]);
+    initial = e.targetTouches[0].pageX;
+    touchobj.StartX = s();
+    console.log(touchobj);
 }
