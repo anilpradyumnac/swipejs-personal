355f1b4aed577d27b539673cd1e219d7d63a14f0
anilpradyumnac <anilpradyumnac@gmail.com>
2016-01-19 17:32:35 +0530

    Version bump

--- public/test.js
+++ public/test.js
@@ -1,18 +1,18 @@
 'use strict'
 
 //Stepper function added from FRPJS
-// function stepper(eventStream, initial) {
-//     var valueAtLastStep = initial, valueAtCurrentStep;
-//     var skipFirst = true;
-//     eventStream(function nextStep(value) {
-//         if (skipFirst) skipFirst = false;
-//         else valueAtLastStep = valueAtCurrentStep;
-//         valueAtCurrentStep = value;
-//     });
-//     return (function behaveAtLastStep() {
-//         return valueAtLastStep
-//     });
-// }
+function stepper(eventStream, initial) {
+    var valueAtLastStep = initial, valueAtCurrentStep;
+    var skipFirst = true;
+    eventStream(function nextStep(value) {
+        if (skipFirst) skipFirst = false;
+        else valueAtLastStep = valueAtCurrentStep;
+        valueAtCurrentStep = value;
+    });
+    return (function behaveAtLastStep() {
+        return valueAtLastStep
+    });
+}
 //FoldP or reduce
 function foldp(eventStream, step, initial) {
     return (function(next) {
@@ -38,13 +38,13 @@ function on(element, name, useCapture) {
     }
 }
 
-// function bind(eventStream, valueToEvent) {
-//     return function(next) {
-//         eventStream(function(value) {
-//             valueToEvent(value)(next)
-//         })
-//     }
-// }
+function bind(eventStream, valueToEvent) {
+    return function(next) {
+        eventStream(function(value) {
+            valueToEvent(value)(next)
+        })
+    }
+}
 function touchHandler(element) {
    const container = document.getElementById(element);
 
@@ -64,20 +64,3 @@ function touchHandler(element) {
 
    touchMove$(value => value);
 }
-// function touchHandler(element) {
-//     const container = document.getElementById(element)
-//     let touchMove$ = on(container, "touchmove", false)
-//     touchMove$ = map(touchMove$, event => ({
-//         pageX: event.targetTouches[0].pageX
-//     }));
-//     touchMove$ = foldp(touchMove$, (prev,curr) => {
-//         console.log(curr, prev);
-//         let dx = curr.pageX - prev.pageX;
-//         let current  = parseFloat(container.style.left) || 0;
-//         console.log(current + dx)
-//         container.style.left = current + dx;
-//         return curr ;
-//     }, { pageX: 0 });
-//     touchMove$(value => value);
-//
-// }
